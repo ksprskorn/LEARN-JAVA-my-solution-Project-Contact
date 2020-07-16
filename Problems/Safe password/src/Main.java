fa69f26cf5ed4137027be6e0589c9990b273b259
ksprskorn <50364388+ksprskorn@users.noreply.github.com>
2020-07-16 17:02:55 +0300

    Update Main.java

--- Problems/Safe password/src/Main.java
+++ Problems/Safe password/src/Main.java
@@ -3,5 +3,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // write your code here
+        Scanner scanner = new Scanner(System.in);
+        String passwors = scanner.nextLine();
+        String regexPatternDigits = ".*\\d.*";
+        String regexPatternLetter = ".*[a-z].*";
+        String regexPatternUpperLetter = ".*[A-Z].*";
+        String regexPatternCorrectLenght = "\\w{12,}";
+
+        boolean contentDigits = passwors.matches(regexPatternDigits);
+        boolean contentLetter = passwors.matches(regexPatternLetter);
+        boolean contentUpperLetter = passwors.matches(regexPatternUpperLetter);
+        boolean correctLenght = passwors.matches(regexPatternCorrectLenght);
+
+        System.out.print((contentDigits & contentLetter & contentUpperLetter & correctLenght) ? "YES" : "NO");
     }
 }
