089be9bf15cc9647e94b444b3f8965ccf87fcf6e
ksprskorn <50364388+ksprskorn@users.noreply.github.com>
2020-07-17 15:19:49 +0300

    Update Main.java

--- Problems/Start or end of a word/src/Main.java
+++ Problems/Start or end of a word/src/Main.java
@@ -8,9 +8,8 @@ public class Main {
         String part = scanner.nextLine();
         String line = scanner.nextLine();
         // write your code here
-        Pattern pattern = Pattern.compile("\\b" + part + "|\\B" + part + "[\\W.,!?]" + "|" + part + "$",Pattern.CASE_INSENSITIVE);
-        //Pattern pattern = Pattern.compile("\\b" + part + "|" + part + "\\b",Pattern.CASE_INSENSITIVE);
-        //Pattern pattern = Pattern.compile("((\\s|\\W|^)" + part + ")|(" + part + "(\\W|\\s|$))|(\\W" + part + "\\W)",Pattern.CASE_INSENSITIVE);
+        Pattern pattern = Pattern.compile("\\b" + part + "|\\B" 
+                          + part + "[\\W.,!?]" + "|" + part + "$", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(line);
 
         System.out.println(matcher.find() ? "YES" : "NO");
