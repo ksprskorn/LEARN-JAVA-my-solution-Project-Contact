a9243f617867a28f4000dbabafaf14982c7331c1
ksprskorn <50364388+ksprskorn@users.noreply.github.com>
2020-07-22 17:29:45 +0300

    Update Main.java

--- Problems/Find all passwords/src/Main.java
+++ Problems/Find all passwords/src/Main.java
@@ -8,7 +8,41 @@ class Main {
         Scanner scanner = new Scanner(System.in);
 
         String text = scanner.nextLine();
-
+        //text = "My email javacoder@gmail.com with password SECRET115. Here is my old PASSWORD: PASS111.";
+        //text = "My email is javacoder@gmail.com."
+        //text = "PASSWORD: 1234567 ABDFSDJFJDFHSIF password:SECRETSECRET and and and oeirjorejtoe 834038053 Pass 2394204204 and PAssWoRd PassWord.";
+        //text = "jgoidejg903u4t093i3pk4f0390358023ofpdsoifgt934580934-24=20492482043 PASSWORD:345730573939urferuighf 875837459837537593hierht934y7598347543";
         // write your code here
+        Pattern pattern = Pattern.compile("password:? *", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+
+        Pattern patternPasword = Pattern.compile("",Pattern.CASE_INSENSITIVE);
+        Matcher matcherPasword = patternPasword.matcher(text);
+
+        if (!matcher.find()) {
+            System.out.println("No passwords found.");
+        } else {
+            matcher.reset();
+        }
+        String textModify;
+        int lastIndex;
+        while (matcher.find()) {
+            textModify = text.substring(matcher.end(),text.length());
+            if (textModify.indexOf(".") < textModify.indexOf(" ")) {
+                lastIndex = textModify.indexOf(".");
+            } else {
+                lastIndex = textModify.indexOf(" ");
+            }
+            if (textModify.indexOf(".") < 0 ) {
+                lastIndex = textModify.indexOf(" ");
+            }
+            if (textModify.indexOf(" ") < 0 ) {
+                lastIndex = textModify.indexOf(".");
+            }
+
+            System.out.println(textModify.substring(0,lastIndex));
+            lastIndex = -1;
+        }
+
     }
 }
