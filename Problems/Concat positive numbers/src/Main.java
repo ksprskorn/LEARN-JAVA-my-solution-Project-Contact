1515c1577a011111bb42c3d51dac3675be9f6625
ksprskorn <50364388+ksprskorn@users.noreply.github.com>
2020-07-25 22:49:22 +0300

    Create Main.java

---
+++ Problems/Concat positive numbers/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+class ConcatPositiveNumbersProblem {
+
+    public static ArrayList<Integer> concatPositiveNumbers(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        ArrayList<Integer> newList = new ArrayList<>();
+        l1.addAll(l2);
+        for (int i : l1) {
+            if (i >= 0) {
+                newList.add(i);
+            }
+        }
+        return newList; // write your code here
+    }
+
+    /* Do not modify this method */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list1 = readArrayList(scanner);
+        ArrayList<Integer> list2 = readArrayList(scanner);
+
+        ArrayList<Integer> result = concatPositiveNumbers(list1, list2);
+
+        result.forEach(n -> System.out.print(n + " "));
+    }
+
+    /* Do not modify this method */
+    private static ArrayList<Integer> readArrayList(Scanner scanner) {
+        return Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+}
