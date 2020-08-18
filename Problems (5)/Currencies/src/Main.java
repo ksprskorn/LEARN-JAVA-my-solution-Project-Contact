fcd2a8b19c8dc7b18a9c7c6513563c1b35459b19
ksprskorn <50364388+ksprskorn@users.noreply.github.com>
2020-08-18 10:30:48 +0300

    Create Main.java

---
+++ Problems (5)/Currencies/src/Main.java
@@ -0,0 +1,18 @@
+// declare your enum here
+enum Currency {
+    USD("United States dollar"),
+    EUR("Euro"),
+    GBP("Pound sterling"),
+    RUB("Russian ruble"),
+    UAH("Ukrainian hryvnia"),
+    KZT("Kazakhstani tenge"),
+    CAD("Canadian dollar"),
+    JPY("Japanese yen"),
+    CNY("Chinese yuan");
+
+    String name;
+
+    Currency(String name) {
+        this.name = name;
+    }
+}
