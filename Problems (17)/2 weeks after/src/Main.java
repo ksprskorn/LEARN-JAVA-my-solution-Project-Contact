433ba1a194c6bed58e15f70ca233a53a994573cc
ksprskorn <50364388+ksprskorn@users.noreply.github.com>
2020-08-20 16:43:50 +0300

    Create Main.java

---
+++ Problems (17)/2 weeks after/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String inputDate = scanner.nextLine();
+        LocalDate date = LocalDate.parse(inputDate);
+        System.out.println(date.plusWeeks(2));
+
+    }
+}
