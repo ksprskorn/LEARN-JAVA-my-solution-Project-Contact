9a051c43676ee8cc2ea25f0add499db04a698cdb
ksprskorn <50364388+ksprskorn@users.noreply.github.com>
2020-08-20 16:43:58 +0300

    Create Main.java

---
+++ Problems (21)/Getting information on a day/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.time.LocalDate;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String inputDate = scanner.next();
+        LocalDate date = LocalDate.parse(inputDate);
+        System.out.println(date.getDayOfYear() + " " + date.getDayOfMonth());
+    }
+}
