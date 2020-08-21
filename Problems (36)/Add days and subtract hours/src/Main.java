3e0385610cac7d0592580d8fc054349b109b1be1
ksprskorn <50364388+ksprskorn@users.noreply.github.com>
2020-08-21 19:39:37 +0300

    Create Main.java

---
+++ Problems (36)/Add days and subtract hours/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String stringDate = scanner.nextLine();
+        LocalDateTime date = LocalDateTime.parse(dateString).plusHours(11);
+        LocalDate shortDate = LocalDate.of(date.)
+        System.out.println(date.toString());
+    }
+}
