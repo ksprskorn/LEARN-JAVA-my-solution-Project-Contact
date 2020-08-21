ff0b6bea3ee005155573728ead3a1e017423627b
ksprskorn <50364388+ksprskorn@users.noreply.github.com>
2020-08-21 19:39:34 +0300

    Update Main.java

--- Problems (35)/Whole hours between two date-time pairs/src/Main.java
+++ Problems (35)/Whole hours between two date-time pairs/src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+import java.time.*;
+import java.lang.Math;
+
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+        String datetimeString1 = scanner.nextLine();
+        String datetimeString2 = scanner.nextLine();
+
+        LocalDateTime dateTime1 = LocalDateTime.parse(datetimeString1);
+        LocalDate date1 = LocalDate.of(dateTime1.getYear(), dateTime1.getMonth(), dateTime1.getDayOfMonth());
+        int days1 = date1.getDayOfYear();
+        LocalTime time1 = LocalTime.of(dateTime1.getHour(), dateTime1.getMinute());
+        long seconds1 = time1.toSecondOfDay();
+
+//        System.out.println(dateTime1.toString());
+//        System.out.println(days1);
+//        System.out.println(seconds1);
+
+        LocalDateTime dateTime2 = LocalDateTime.parse(datetimeString2);
+        LocalDate date2 = LocalDate.of(dateTime2.getYear(), dateTime2.getMonth(), dateTime2.getDayOfMonth());
+        int days2 = date2.getDayOfYear();
+        LocalTime time2 = LocalTime.of(dateTime2.getHour(), dateTime2.getMinute());
+        long seconds2 = time2.toSecondOfDay();
+
+//        System.out.println(dateTime2.toString());
+//        System.out.println(days2);
+//        System.out.println(seconds2);
+
+        int daysToHours = Math.abs(days1 - days2) * 24;
+//        System.out.println(daysToHours);
+        double hoursLeft = (double)(seconds1 - seconds2) / 3600;
+
+        System.out.println((int) ((double) daysToHours - hoursLeft) );
+
+
     }
 }
